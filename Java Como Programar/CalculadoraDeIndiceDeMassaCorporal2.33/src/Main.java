80f4dc9fdebdd91924d7c10d9eae3275006caad4
victorteixeirasilva <victorteixeira2308@gmail.com>
2025-01-17 12:25:27 -0300

    Copiando Enunciado Exercícios

---
+++ Java Como Programar/CalculadoraDeIndiceDeMassaCorporal2.33/src/Main.java
@@ -0,0 +1,24 @@
+/*
+    @Autor: Victor Teixeira Silva
+
+    Introduzimos a calculadora de índice de massa corporal (IMC) no Exercício 1.10. As fórmulas para calcular o IMC são
+IMC = pesoEmLibras X 703 / alturaEmPolegadas^2 ou IMC = pesoEmQuilogramas / alturaEmMetros^2 Crie um aplicativo de
+calculadora IMC que leia o peso do usuário em libras e a altura em polegadas (ou, se prefirir, o peso em quilogramas a
+altura em metros) e, então calcule exiba o índice de massa corporal dele. Além disso, que exiba as seguintes informações
+do department of Health and Human Services/National Institutes of Health, assim o usuário pode avaliar o seu IMC:
+================================
+BMI VALUES
+Underweight: less than 18.5
+Normal: between 18.5 and 24.9
+Overweight: beetween 25 and 29.9
+Obese: 30 or greater
+================================
+
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    }
+}
---
+++ Java Como Programar/ComputadorizacaoDosRegistrosDeSaude3.17/src/Main.java
@@ -0,0 +1,27 @@
+/*
+    @Autor Victor Teixeira Silva
+
+    Uma questão relacionada à assistência médica discutida ultimamente nos veículos de comunicação é a computadorização
+dos registros de saúde. Essa possibilidade está sendo abordada de maneira cautelosa por causa da preocupação quanto à
+privacidade e à segurança de dadds sigilosos, entre outros motivos. [Iremos discutir essas preocupações em exercícios
+posteriores.] A computadorização dos registros de saúde pode facilitar que pacientes compartilhem seus perfis e histórico
+de saúde entre vários profissionais de saúde. Isso talvez aprimore a qualidade da assistência médica, ajude a evitar
+conflitos e prescrições erradas de medicamentos. reduza custos ambulatórios e salve vidas. Neste exercício, você projetará
+uma classe HealthProfile "inicial" para uma pessoa. Os atributos da classe devem incluir nome, sexo, data de nascimento
+(consistindo em atributos separados para mês, dia e ano de nascimento), altura (em metros) e peso (em quilogramas) da pessoa.
+Sua classe deve ter um construtor que receba esses dados. Para cada atributo, forneça métodos set e get. A classe também
+deve incluir métodos que calculem e retornem a idade do usuário em anos, intervalo de frequência cardíaca máxima e
+frequencia cardíaca alvo (Veja o Exercício 3.16), além de massa corporal (IMC; veja o Exercício 2.33). Escreva um aplicativo
+Java que solicite as informações da pessoa, instancie um objeto da classe HealthProfile para ela e imprima as informações
+a partir desse objeto - Incluindo nome, sobrenome, sexo, data de nascimento, altura e peso da pessoa -, e então calcule e
+imprima a idadde em anos, IMC, intervalo de frequência cardíaca máxima e frequência cardíaca alvo. Ele também deve exibir
+o gráfico de valores IMC do Exercício 2.33
+
+Use JOption
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
