950aec04ddb0549be824a86db4e009b1a5483f32
victorteixeirasilva <victorteixeira2308@gmail.com>
2025-01-17 09:15:20 -0300

    3.14

---
+++ Java Como Programar/ClasseDate3.14/src/Date.java
@@ -0,0 +1,49 @@
+/*
+@Autor Victor Teixeira Silva
+
+(CLasse Date) Crie uma classe chamada Date que inclua três variáveis de instância - Mês (tipo int), dia (tipo int)
+e ano (tipo int). Forneça construtor que inicializa as três variáveis de instância supondo que os valores fornecidos
+estejam corretos. Ofereça um método set e get para cada variável de instância. Apresente um método displayDate que
+exiba mês, dia e ano separados por barras normais (/). Escreva um aplicativo de teste chamado DateTest que demonstre as
+capacidades da classe Date.
+ */
+public class Date {
+
+    private int month;
+    private int day;
+    private int year;
+
+    public Date(int month, int day, int year){
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+    public void displayDate(){
+        System.out.println(this.day+"/"+this.month+"/"+this.year);
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ Java Como Programar/ClasseDate3.14/src/DateTest.java
@@ -0,0 +1,12 @@
+public class DateTest {
+    public static void main(String[] args) {
+
+        Date date = new Date(8, 23, 2003);
+        Date date1 = new Date(1, 17, 2025);
+
+        date.displayDate();
+        date1.displayDate();
+
+
+    }
+}
