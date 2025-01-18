b16b9a070b88b433f6b6d2db6adaafe3f05c7338
victorteixeirasilva <victorteixeira2308@gmail.com>
2025-01-18 19:11:03 -0300

    Copiando Enunciado Exercício 3.17

--- Java Como Programar/ComputadorizacaoDosRegistrosDeSaude3.17/src/Main.java
+++ Java Como Programar/ComputadorizacaoDosRegistrosDeSaude3.17/src/Main.java
@@ -3,7 +3,7 @@
 
     Uma questão relacionada à assistência médica discutida ultimamente nos veículos de comunicação é a computadorização
 dos registros de saúde. Essa possibilidade está sendo abordada de maneira cautelosa por causa da preocupação quanto à
-privacidade e à segurança de dadds sigilosos, entre outros motivos. [Iremos discutir essas preocupações em exercícios
+privacidade e à segurança de dados sigilosos, entre outros motivos. [Iremos discutir essas preocupações em exercícios
 posteriores.] A computadorização dos registros de saúde pode facilitar que pacientes compartilhem seus perfis e histórico
 de saúde entre vários profissionais de saúde. Isso talvez aprimore a qualidade da assistência médica, ajude a evitar
 conflitos e prescrições erradas de medicamentos. reduza custos ambulatórios e salve vidas. Neste exercício, você projetará
@@ -17,7 +17,36 @@ a partir desse objeto - Incluindo nome, sobrenome, sexo, data de nascimento, alt
 imprima a idadde em anos, IMC, intervalo de frequência cardíaca máxima e frequência cardíaca alvo. Ele também deve exibir
 o gráfico de valores IMC do Exercício 2.33
 
-Use JOption
+Use JOptionPane
+
+Anotações para solução:
+
+    Escreva um aplicativo Java que solicite as informações da pessoa, instancie um objeto da classe HealthProfile para
+ela e imprima as informações a partir desse objeto - Incluindo nome, sobrenome, sexo, data de nascimento, altura e peso
+da pessoa -, e então calcule e imprima a idadde em anos, IMC, intervalo de frequência cardíaca máxima e frequência
+cardíaca alvo. Ele também deve exibir o gráfico de valores IMC do Exercício 2.33
+
+    1- Crie a Classe DateOfBirth semelhante a do 3.16
+
+    2- A classe também deve incluir métodos que calculem e retornem a idade do usuário em anos
+
+    2- Crie a classe HealthProfile "inicial" para uma pessoa.
+
+    3- Os atributos da classe devem incluir nome, sexo, data de nascimento, altura (em metros) e peso (em quilogramas) da pessoa.
+
+    4- Sua classe deve ter um construtor que receba esses dados. Para cada atributo
+
+    5- forneça métodos set e get.
+
+    6- Adapte a classe HeartRates para Usar o HealthProfile para os calculos.
+
+    7- Crie a classe Imc adptando o código do exercicio 2.33.
+
+    8- Crie um Front-End em JOptionPane que solicite as informações (Nome, Sobrenome, Sexo, Data de Nascimento, Altura(Metros) e Peso(Quilogramas)).
+
+    9- Crie um Front-End em JOptionPane que mostre as informações pessoais
+    (Nome, Sobrenome, Sexo, Data de Nascimento, Altura, Peso, Idade em Anos, IMC, frequência cardiaca maxima, Intervalo de frequencia alvo e Grafico de valores IMC).
+
 
  */
 public class Main {
