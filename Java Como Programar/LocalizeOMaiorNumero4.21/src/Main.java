83d5902ec6a0c3edf4f6db99180b96bcedf64334
victorteixeirasilva <victorteixeira2308@gmail.com>
2025-01-21 23:40:38 -0300

    4.21

--- Java Como Programar/LocalizeOMaiorNumero4.21/src/Main.java
+++ Java Como Programar/LocalizeOMaiorNumero4.21/src/Main.java
@@ -9,13 +9,50 @@ todos os 10 números foram processados).
         b) number: o inteiro mais recentemente inserido pelo usuário.
         c) largest: 0 maior número encontrado até agora
 
+Anotações:
+
+    Sistema inicia as vaiaveis de contador, ultimo número e número mais alto
+
+    Repita 10 vezes
+        sistema recebe o número
+        sistema verifica se esse novo número é maior que o número mais alto
+            se for ele toma o lugar do número mais alto
+        se não ele toma o lugar do último número
+    Fim Repita
+
+    Sistema informa o número mais alto entre os 10
+
+
+
  */
+
+import java.util.Scanner;
+
 /**
  * @author Victor Teixeira Silva
  * @version 1.0
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 1;
+        int number = 0;
+        int largest = 0;
+
+        while (counter <= 10) {
+            System.out.println("Informe um número:");
+            number = scanner.nextInt();
+
+            if (number > largest) {
+                largest = number;
+            }
+
+            counter++;
+        }
+
+        System.out.println("O maior número é: "+largest);
+
+
     }
 }
