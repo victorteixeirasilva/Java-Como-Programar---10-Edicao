cbd3ecaedd624cad69f27e4ec2c039d8c5e8428e
victorteixeirasilva <victorteixeira2308@gmail.com>
2025-01-22 22:51:27 -0300

    4.23 Pseudocódigo

--- Java Como Programar/EncontreOsDoisNumerosMaiores4.23/src/Main.java
+++ Java Como Programar/EncontreOsDoisNumerosMaiores4.23/src/Main.java
@@ -3,6 +3,17 @@
     (Encontre os dois números maiores) Utilizando uma abordagem semelhante àquela do Exercício 4.21, encontre os dois
 maiores valores entre os 10 valores inseridos [Observação: você só pode inserir cada número uma vez.]
 
+Anotações:
+
+    Repita 10 vezes
+        Sistema solicita entrada do número
+        Sistema verifica se o número já não foi inserido
+            Se não foi, sistema adiciona a lista.
+        Se foi, solicitar novo número.
+    Fim Repita
+
+    Sistema compara todos os números adicionado a lista e imprime os dois maiores
+
  */
 /**
  * @author Victor Teixeira Silva
