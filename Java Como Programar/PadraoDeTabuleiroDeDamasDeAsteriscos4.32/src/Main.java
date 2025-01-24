780e862b0b6ae9bb3dd8ce7ec84f6d4421a0f15f
victorteixeirasilva <victorteixeira2308@gmail.com>
2025-01-24 13:28:00 -0300

    4.32

--- Java Como Programar/PadraoDeTabuleiroDeDamasDeAsteriscos4.32/src/Main.java
+++ Java Como Programar/PadraoDeTabuleiroDeDamasDeAsteriscos4.32/src/Main.java
@@ -23,6 +23,17 @@ que o programa gere saída de um único caractere de nova linha. [Dica: as instr
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        for (int i = 1; i <= 4; i++){
+            for (int a = 1; a <= 8; a++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            for (int b = 1; b <= 8; b++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
     }
 }
