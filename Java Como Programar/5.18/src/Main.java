b2ee4f0c53df6d10f01981cc72418aac172dce60
victorteixeirasilva <victorteixeira2308@gmail.com>
2025-02-04 18:16:13 -0300

    5.18

---
+++ Java Como Programar/5.18/src/Main.java
@@ -0,0 +1,15 @@
+/*
+    5.18 - (Programa de Juros compostos modificado) Modifique o aplicativo na Figura 5.6 para utilizar apenas inteiros
+    para calcular os juros compostos. [Dica: trate todas as quantidades monetárias como números inteiros em centavos.
+    Então, divida o resultado nas suas partes dólar e centavos utilizando as operações divisão e resto, respectivamente.
+    Insira uma vírgula entre as partes dólar e centavos.]
+ */
+/**
+ * @author Victor Teixeira Silva
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
