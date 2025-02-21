271a20c52576ebacc076788b2f51ff49ee7be37e
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-02-21 14:29:41 -0300

    7.18

--- Java Como Programar/7.18/src/Main.java
+++ Java Como Programar/7.18/src/Main.java
@@ -30,6 +30,7 @@ public class Main {
     private static final int BOX_CARS = 12;
 
     private static int[] vitorias = new int[21];
+    private static int[] derrotas = new int[21];
 
     // joga uma partida de craps
     public static void main(String[] args) {
@@ -53,6 +54,7 @@ public class Main {
                 case TREY: // perde com 3 no primeiro lançamento
                 case BOX_CARS: // perde com 12 no primeiro lançamento
                     gameStatus = Status.LOST;
+                    derrotas[0]++;
                     break;
                 default: // não ganhou nem perdeu, portanto registra a pontuação
                     gameStatus = Status.CONTINUE; // jogo não terminou
@@ -76,6 +78,7 @@ public class Main {
                     if (sumOfDice == SEVEN) // perde obtendo 7 antes de atingir a pontuação
                     {
                         gameStatus = Status.LOST;
+                        addDerrotas(count);
                     }
                 }
                 count++;
@@ -89,7 +92,7 @@ public class Main {
         }
 
         System.out.println();
-        System.out.println("***         Resultado       ***");
+        System.out.println("***   A - Resultado Vitórias   ***");
 
         System.out.println("Rolagem     |       Vitórias");
 
@@ -97,6 +100,112 @@ public class Main {
             System.out.println("  " + i + "       |      " + vitorias[i-1] + "  ");
         }
 
+        System.out.println();
+        System.out.println("***   B - Resultado Derrotas   ***");
+
+        System.out.println("Rolagem     |       Derotas");
+
+        for (int i = 1; i <= 20; i++){
+            System.out.println("  " + i + "       |      " + derrotas[i-1] + "  ");
+        }
+
+        System.out.println();
+        System.out.println("C) Quais são as chances de ganhar no jogo de dado?\n" +
+                "\n" +
+                "O Craps é um dos jogos de azar mais comuns em cassinos, o que significa que, apesar da aparente aleatoriedade, a casa (o cassino) tem uma vantagem estatística. No entanto, o Craps oferece algumas das melhores chances de ganhar em comparação com outros jogos de cassino, especialmente se você entender as apostas mais favoráveis.\n" +
+                "\n" +
+                "As chances de ganhar no Craps variam dependendo do tipo de aposta que você faz. Algumas apostas têm uma chance maior de ganhar, mas pagam menos, enquanto outras têm uma chance menor, mas pagam mais.\n" +
+                "\n" +
+                "Apostas na linha de passe/não passe: São as apostas mais básicas e têm uma chance de ganhar de aproximadamente 49%. A vantagem da casa nessas apostas é de cerca de 1,41%.\n" +
+                "Apostas de probabilidades: São apostas adicionais que você pode fazer após ter feito uma aposta na linha de passe/não passe. Essas apostas não têm vantagem da casa, o que significa que suas chances de ganhar são iguais às chances da casa. No entanto, você só pode fazer essas apostas se já tiver feito uma aposta na linha de passe/não passe, que tem uma vantagem da casa.\n" +
+                "Outras apostas: Existem muitas outras apostas no Craps, mas a maioria delas tem uma vantagem da casa maior do que as apostas na linha de passe/não passe e as apostas de probabilidades.\n" +
+                "O que significa o Craps ser um dos jogos mais comuns de cassino?\n" +
+                "\n" +
+                "A popularidade do Craps significa que ele atrai muitos jogadores, o que, por sua vez, gera mais receita para o cassino. A relativa simplicidade das regras e a emoção do jogo contribuem para sua popularidade.\n" +
+                "\n" +
+                "D) Qual é a duração média de um jogo de dados Craps?\n" +
+                "\n" +
+                "A duração média de um jogo de Craps pode variar muito dependendo de vários fatores, como o número de jogadores, a velocidade do jogo e a sorte dos jogadores. No entanto, um jogo típico de Craps pode durar de alguns minutos a várias horas.\n" +
+                "\n" +
+                "E) As chances de ganhar aumentam com a duração do jogo?\n" +
+                "\n" +
+                "Não, as chances de ganhar no Craps não aumentam com a duração do jogo. Cada lançamento dos dados é um evento independente, o que significa que os resultados anteriores não afetam os resultados futuros. Se você fizer uma aposta com uma chance de ganhar de 49%, essa chance permanecerá a mesma, independentemente de você ter jogado por alguns minutos ou várias horas.\n" +
+                "\n" +
+                "Em resumo:\n" +
+                "\n" +
+                "O Craps oferece algumas das melhores chances de ganhar em comparação com outros jogos de cassino.\n" +
+                "As chances de ganhar variam dependendo do tipo de aposta que você faz.\n" +
+                "A duração do jogo não afeta suas chances de ganhar.\n" +
+                "Lembre-se:\n" +
+                "\n" +
+                "O Craps é um jogo de azar e a casa sempre tem uma vantagem estatística.\n" +
+                "Jogue com responsabilidade e aposte apenas o que você pode perder.\n" +
+                "Entenda as regras e as diferentes apostas antes de começar a jogar.");
+
+    }
+
+    public static void addDerrotas(int count){
+        switch (count) {
+            case 2:
+                derrotas[1]++;
+                break;
+            case 3:
+                derrotas[2]++;
+                break;
+            case 4:
+                derrotas[3]++;
+                break;
+            case 5:
+                derrotas[4]++;
+                break;
+            case 6:
+                derrotas[5]++;
+                break;
+            case 7:
+                derrotas[6]++;
+                break;
+            case 8:
+                derrotas[7]++;
+                break;
+            case 9:
+                derrotas[8]++;
+                break;
+            case 10:
+                derrotas[9]++;
+                break;
+            case 11:
+                derrotas[10]++;
+                break;
+            case 12:
+                derrotas[11]++;
+                break;
+            case 13:
+                derrotas[12]++;
+                break;
+            case 14:
+                derrotas[13]++;
+                break;
+            case 15:
+                derrotas[14]++;
+                break;
+            case 16:
+                derrotas[15]++;
+                break;
+            case 17:
+                derrotas[16]++;
+                break;
+            case 18:
+                derrotas[17]++;
+                break;
+            case 19:
+                derrotas[18]++;
+                break;
+            case 20:
+                derrotas[19]++;
+                break;
+            default:
+                derrotas[20]++;
+        }
     }
 
     public static void addVitorias(int count){
