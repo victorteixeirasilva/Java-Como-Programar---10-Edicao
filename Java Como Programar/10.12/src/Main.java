c84e3746e21d5647f7e52e0ddaf5352fc5aa9296
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-08 18:15:28 -0300

    Adicionando Classes

---
+++ Java Como Programar/10.12/src/Main.java
@@ -0,0 +1,14 @@
+//Modifique o sistema de folha de pagamento das figuras 10.4 a 10.9 para
+// incluir a variável de instância private birthDate na classe Employee.
+// Utilize a classe Date da Figura 8.7 para representar o aniversário de
+// um funcionário. Adicione métodos get à classe Date. Suponha que a folha
+// de pagamento seja processada uma vez por mês. Crie um array de variáveis
+// Employee para armazenar referências aos vários objetos de funcionário.
+// Em um loop, calcule a folha de pagamento para cada Employee (polimorficamente)
+// e adicione um bônus de US$ 100.00 à quantia da folha de pagamento do funcionário
+// se o mês atual for aquele em que ocorre o aniversário do Employee.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
